8abfc26415d096505cfdfd404c9ad4e41189c576
Admin <Admin@DESKTOP-SQR286K>
2022-06-14 22:00:14 +0530

    first commit

---
+++ day 2/src/AreaCircle.java
@@ -0,0 +1,18 @@
+
+
+public class AreaCircle 
+{
+	public static void main(String[] args)
+	{
+		int r = 12;
+		float area;
+		area = 3.14f * (r * r) ;
+		System.out.println("Radius = " + r + "  Area of Circle = " +area);
+		
+	}
+
+}
+
+
+	
+
---
+++ day 2/src/AreaTriangle.java
@@ -0,0 +1,17 @@
+
+
+
+public class AreaTriangle 
+{
+	
+	public static void main(String args[])
+	{
+		
+	int b = 4, h=20;
+	float  area;
+	area = 1/2f * (b * h);
+	System.out.println("Area of Triangle = "+ area );
+	
+}
+}
+
