ccdd32fd18c07f81dfacf90ec95012d20a684e52
Admin <Admin@DESKTOP-SQR286K>
2022-08-22 23:29:15 +0530

    1

---
+++ Tns/src/com/tns/uml/Uml.java
@@ -0,0 +1,23 @@
+package com.tns.uml;
+
+public class Uml {
+	
+
+		class Car {
+		    private String color;
+		    private int weight;
+		    private Brand brand_name;
+		}
+
+		class Brand {
+		    private String Skoda;
+		    private String BMW;
+		    private Location location;
+		}
+
+		class Location {
+		    private String US;
+		    private String EU;
+		}
+
+		}
