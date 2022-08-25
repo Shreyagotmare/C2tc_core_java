3b555724903401c232b0eba8d865ef01892fce3c
Admin <Admin@DESKTOP-SQR286K>
2022-08-25 21:38:36 +0530

    .

---
+++ Comparable/src/com/tns/comparator/Employee.java
@@ -0,0 +1,22 @@
+package com.tns.comparator;
+
+public class Employee {
+	private int age;
+	private String name;
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
